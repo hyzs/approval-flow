2f6a6addd90675a6e64ebef16d1590685e22642d
e <xianzhen.liu@igen-tech.com>
2020-11-19 11:07:32 +0800

    init

---
+++ src/main/java/com/github/hyzs/approvalflow/ApprovalFlowApplication.java
@@ -0,0 +1,13 @@
+package com.github.hyzs.approvalflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApprovalFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApprovalFlowApplication.class, args);
+    }
+
+}
